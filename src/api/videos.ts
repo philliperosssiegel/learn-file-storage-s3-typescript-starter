67e2ae6e5d9749ef21164b1864724593b11f220c
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2026-01-06 18:01:26 -0500

    Completed CH 3 L3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -2,7 +2,75 @@ import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { getAssetDiskPath, getAssetPath } from "./assets";
+import { randomBytes } from "crypto";
+import { rm } from "node:fs/promises";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("Uploading video", videoId, "by user", userID);
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+      throw new NotFoundError("Video not found");
+  }
+
+  if (video.userID !== userID) {
+      throw new UserForbiddenError("User is not the owner of this video");
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Video file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 1 << 30; // 1 GB
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(`Video file exceeds maximum upload size of ${MAX_UPLOAD_SIZE} (bytes)`);
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for video");
+  }
+
+  if (mediaType != "video/mp4") {
+    throw new BadRequestError("Invalid video, expecting MP4 media file")
+  }
+
+  // Write the video asset (temporarily) to local storage
+  const assetPath = getAssetPath(mediaType);
+  const assetDiskPath = getAssetDiskPath(cfg, assetPath); 
+
+  await Bun.write(assetDiskPath, file);
+
+  //S3 handling
+
+  // await cfg.s3Client.file(assetDiskPath);
+  const key = `${randomBytes(32).toString("hex")}.mp4`
+  // await cfg.s3Client.file(key, cfg.s3Bucket).write(Bun.file(assetDiskPath), mediaType);
+
+  await cfg.s3Client.file(key, { bucket: cfg.s3Bucket }).write(Bun.file(assetDiskPath), { type: mediaType})
+  
+  // Delete the temporary video asset from local storage
+  await rm(assetDiskPath, {force: true });
+
+  // Update the videoURL in the database with the S3 bucket and key
+  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}` //getAssetURL(cfg, assetPath);
+  video.videoURL = videoURL;
+
+  await updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import { newDatabase } from "./db/db";
 import type { Database } from "bun:sqlite";
+import { S3Client } from "bun";
 
 export type ApiConfig = {
   db: Database;
@@ -7,20 +8,29 @@ export type ApiConfig = {
   platform: string;
   filepathRoot: string;
   assetsRoot: string;
+  s3Client: S3Client;
   s3Bucket: string;
   s3Region: string;
   s3CfDistribution: string;
   port: string;
 };
 
+
+
 const pathToDB = envOrThrow("DB_PATH");
 const jwtSecret = envOrThrow("JWT_SECRET");
 const platform = envOrThrow("PLATFORM");
 const filepathRoot = envOrThrow("FILEPATH_ROOT");
 const assetsRoot = envOrThrow("ASSETS_ROOT");
+// const s3client: S3Client = s3;
 const s3Bucket = envOrThrow("S3_BUCKET");
 const s3Region = envOrThrow("S3_REGION");
 const s3CfDistribution = envOrThrow("S3_CF_DISTRO");
+const s3client = new S3Client({
+  accessKeyId: envOrThrow("AWS_ACCESS_KEY_ID"),
+  secretAccessKey: envOrThrow("AWS_SECRET_ACCESS_KEY"),
+  region: s3Region,  
+})
 const port = envOrThrow("PORT");
 
 const db = newDatabase(pathToDB);
@@ -31,6 +41,7 @@ export const cfg: ApiConfig = {
   platform: platform,
   filepathRoot: filepathRoot,
   assetsRoot: assetsRoot,
+  s3Client: s3client,
   s3Bucket: s3Bucket,
   s3Region: s3Region,
   s3CfDistribution: s3CfDistribution,
@@ -44,3 +55,7 @@ function envOrThrow(key: string) {
   }
   return envVar;
 }
+
+
+console.log("AWS_ACCESS_KEY_ID from env:", process.env.AWS_ACCESS_KEY_ID);
+console.log("AWS_SECRET_ACCESS_KEY from env:", process.env.AWS_SECRET_ACCESS_KEY);
